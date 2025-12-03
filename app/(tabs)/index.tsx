42b5cec6f62170901f3856935f1ca02b6a2ff08a
samueldob <frankdobisamuel@gmail.com>
2025-12-03 22:59:24 +0100

    add: replaced files to solve version mismatch issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
+import { useRouter } from 'expo-router';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
+// import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
+  const router =  useRouter();
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Tab One On One </Text>
+      <Button  
+          title="Go to Profile"
+          onPress={()=>router.push("/two")}
+         />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
     </View>
   );
 }
