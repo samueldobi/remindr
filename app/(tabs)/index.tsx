7104a126abf08561a26a8c710c0d99b0601ade6c
samueldob <frankdobisamuel@gmail.com>
2025-12-16 15:23:05 +0100

    add:created the categories component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,73 @@
-import { StyleSheet, Button , Image} from 'react-native';
+import { StyleSheet, Button , Image, TouchableWithoutFeedback, Keyboard, Pressable,ScrollView} from 'react-native';
 import { useRouter } from 'expo-router';
+import { Searchbar } from 'react-native-paper';
+import { useState } from 'react';
+import { categories } from '@/constants/Categories';
 
 // import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View  } from '@/components/Themed';
+// import { ScrollView } from 'react-native/types_generated/index';
 
 export default function TabOneScreen() {
   const router =  useRouter();
+  const [query, setQuery] = useState('');
   return (
-    <View style={styles.container}>
-      <View style={styles.headerView}>
-        <View>
-          <Text style={styles.headerTitle}>Welcome Julia</Text>
-          <Text style={styles.headerSubtitle}>8 pending bills</Text>
-        </View>
-        <View>
-          <Image
-          source={require('../../assets/images/woman.png')}
-          style={styles.headerImage}
-          resizeMode="contain"
-          />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        {/*  Header View */}
+        <View style={styles.headerView}>
+          <View>
+            <Text style={styles.headerTitle}>Welcome Julia</Text>
+            <Text style={styles.headerSubtitle}>8 pending bills</Text>
+          </View>
+          <View>
+            <Image
+            source={require('../../assets/images/woman.png')}
+            style={styles.headerImage}
+            resizeMode="contain"
+            />
 
+          </View>
         </View>
+
+        {/* Searchbar View */}
+        <View>
+            <Searchbar
+          placeholder="Search  reminder"
+          onChangeText={setQuery}
+          value={query}
+          style={styles.searchBar}
+        />
+        </View>
+
+        {/* Categories View */}
+        <View>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={true}
+
+          >
+            {categories.map((category)=>(
+              <Pressable
+              key={category.id}
+              >
+                <Text>{category.name}</Text>
+              </Pressable>
+            ))}
+          </ScrollView>
+        </View>
+        {/* <View>
+          <Text>Welcome Julia</Text>
+        </View>
+        <Text >Welcome to the Home Screen</Text>
+        <Button  
+            title="Go to Profile"
+            onPress={()=>router.push("/two")}
+          /> */}
+        {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
+        {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
       </View>
-      {/* <View>
-        <Text>Welcome Julia</Text>
-      </View>
-      <Text >Welcome to the Home Screen</Text>
-      <Button  
-          title="Go to Profile"
-          onPress={()=>router.push("/two")}
-         /> */}
-      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
-      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -41,6 +76,7 @@ const styles = StyleSheet.create({
     flex:1,
     justifyContent:'flex-start',
     marginTop:60,
+    padding:10,
 
   },
   headerView:{
@@ -61,6 +97,9 @@ const styles = StyleSheet.create({
   headerImage:{
     width:50,
     height:50,
+  },
+  searchBar:{
+    color:'#0BA6DF',
   }
   // container: {
   //   flex: 1,
---
+++ constants/Categories.ts
@@ -0,0 +1,7 @@
+export const categories = [
+  { id: '1', name: 'Work' },
+  { id: '2', name: 'School' },
+  { id: '3', name: 'Personal' },
+  { id: '4', name: 'Health' },
+  { id: '5', name: 'Finance' },
+];
